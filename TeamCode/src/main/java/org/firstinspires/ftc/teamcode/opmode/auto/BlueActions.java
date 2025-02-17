27cdf1236bcaa103e936f988a701cecb14275c5a
lukewhite32 <lukebenwhite32@gmail.com>
2025-02-16 21:06:12 -0500

    finish 6 sample trajectories, working on 5 spec w side claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueActions.java
@@ -13,35 +13,51 @@ import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 
 // This class is all the actions used in autonomous. They are all in one file so you only have to change one action to affect all autos.
 public class BlueActions {
-    public static final Pose basketPos = new Pose(15, 129, Math.toRadians(315));
-    public static final Pose rightmostSamplePose = new Pose(23.8, 120.35, Math.toRadians(0));
-    public static final Pose middleSamplePose = new Pose(basketPos.getX(), basketPos.getY(), Math.toRadians(0));
-    public static final Pose leftmostSamplePose = new Pose(27.6, 131.35, Math.toRadians(25));
+    public static final Pose basketPos = new Pose(
+            15,
+            129,
+            Math.toRadians(315)
+    );
+    public static final Pose rightmostSamplePose = new Pose(
+            23.8,
+            120.35,
+            Math.toRadians(0)
+    );
+    public static final Pose middleSamplePose = new Pose(
+            basketPos.getX(),
+            basketPos.getY(),
+            Math.toRadians(0)
+    );
+    public static final Pose leftmostSamplePose = new Pose(
+            27.6,
+            131.35,
+            Math.toRadians(25)
+    );
 
-    public static final double specimenPlaceY = -32.25;
+//    public static final double specimenPlaceY = -32.25;
+//
+//    public static final Pose leftSpecimenPos   = new Pose(-12, specimenPlaceY, Math.toRadians(180));
+//    public static final Pose rightSpecimenPos  = new Pose(4, specimenPlaceY, Math.toRadians(180));
+//    public static final Pose pickupSpecimenPos = new Pose(45, -66, Math.toRadians(0));
 
-    public static final Pose leftSpecimenPos   = new Pose(-12, specimenPlaceY, Math.toRadians(180));
-    public static final Pose rightSpecimenPos  = new Pose(4, specimenPlaceY, Math.toRadians(180));
-    public static final Pose pickupSpecimenPos = new Pose(45, -66, Math.toRadians(0));
-
+    // Sample Paths
 
     public static PathChain startToBasket(Drivetrain drive, Pose home) {
         return drive.getBuilder()
-                .addPath(new BezierLine(new Point(home), new Point(basketPos)))
+                .addPath(new BezierLine(
+                        new Point(home),
+                        new Point(basketPos)
+                ))
                 .setLinearHeadingInterpolation(home.getHeading(), basketPos.getHeading())
-                .build();
-    }
-
-    public static PathChain startToSpecimen(Drivetrain drive, Pose home, Pose specPos) {
-        return drive.getBuilder()
-                .addPath(new BezierLine(new Point(home), new Point(specPos)))
-                .setLinearHeadingInterpolation(home.getHeading(), specPos.getHeading())
                 .build();
     }
 
     public static PathChain basketToFirstSample(Drivetrain drive) {
         return drive.getBuilder()
-                .addPath(new BezierLine(new Point(basketPos), new Point(rightmostSamplePose)))
+                .addPath(new BezierLine(
+                        new Point(basketPos),
+                        new Point(rightmostSamplePose)
+                ))
                 .setLinearHeadingInterpolation(basketPos.getHeading(), rightmostSamplePose.getHeading())
                 .build();
     }
@@ -54,7 +70,10 @@ public class BlueActions {
 
     public static PathChain basketToThirdSample(Drivetrain drive) {
         return drive.getBuilder()
-                .addPath(new BezierLine(new Point(basketPos), new Point(leftmostSamplePose)))
+                .addPath(new BezierLine(
+                        new Point(basketPos),
+                        new Point(leftmostSamplePose)
+                ))
                 .setLinearHeadingInterpolation(basketPos.getHeading(), leftmostSamplePose.getHeading())
                 //.strafeToLinearHeading(new Vector2d(-44, -37), Math.toRadians(152))
                 .build();
@@ -64,29 +83,59 @@ public class BlueActions {
         return drive.getBuilder()
                 .addPath(new BezierCurve(
                         new Point(15.000, 129.000, Point.CARTESIAN),
-                        new Point(61.430, 132.845, Point.CARTESIAN),
-                        new Point(59.765, 96.221, Point.CARTESIAN)
+                        new Point(57.800, 132.845, Point.CARTESIAN),
+                        new Point(60.200, 96.221, Point.CARTESIAN)
                 ))
+                .setLinearHeadingInterpolation(basketPos.getHeading(), Math.toRadians(270))
                 .build();
     }
 
     public static PathChain submersibleToBasket(Drivetrain drive) {
         return drive.getBuilder()
-                //.splineToLinearHeading(new Pose2d(basketPos, Math.toRadians(45)), Math.toRadians(240))
+                .addPath(new BezierCurve(
+                        new Point(60.200, 96.221, Point.CARTESIAN),
+                        new Point(57.800, 132.845, Point.CARTESIAN),
+                        new Point(15.000, 129.000, Point.CARTESIAN)
+                ))
+                .setLinearHeadingInterpolation(Math.toRadians(270), basketPos.getHeading())
                 .build();
     }
 
     public static PathChain basketToSubmersible2(Drivetrain drive) {
         return drive.getBuilder()
-                //.splineTo(new Vector2d(-21, -12), Math.toRadians(0))
+                .addPath(new BezierCurve(
+                        new Point(15.000, 129.000, Point.CARTESIAN),
+                        new Point(61.430, 132.845, Point.CARTESIAN),
+                        new Point(64.500, 96.221, Point.CARTESIAN)
+                ))
+                .setLinearHeadingInterpolation(basketPos.getHeading(), Math.toRadians(270))
                 .build();
     }
 
     public static PathChain submersible2ToBasket(Drivetrain drive) {
         return drive.getBuilder()
-                //.splineToLinearHeading(new Pose2d(basketPos, Math.toRadians(45)), Math.toRadians(240))
+                .addPath(new BezierCurve(
+                        new Point(64.500, 96.221, Point.CARTESIAN),
+                        new Point(61.430, 132.845, Point.CARTESIAN),
+                        new Point(15.000, 129.000, Point.CARTESIAN)
+                ))
+                .setLinearHeadingInterpolation(Math.toRadians(270), basketPos.getHeading())
                 .build();
     }
+
+    // Specimen Paths
+
+    public static PathChain startToSpecimen(Drivetrain drive, Pose home) {
+        return drive.getBuilder()
+                .addPath(new BezierLine(
+                        new Point(home),
+                        new Point(specPos)
+                ))
+                .setLinearHeadingInterpolation(home.getHeading(), specPos.getHeading())
+                .build();
+    }
+
+    public static PathChain
 
 //    public static Action toFirstSample(Drivetrain drive) {
 //         return drive.getBuilder(new Pose2d(rightSpecimenPos, Math.toRadians(180)))
